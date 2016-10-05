685e99ba92d703456bf6a1cb83e98632b1866ac4
jcooley-hylasoft <j.cooley@hylasoftusa.com>
2016-10-05 08:18:29 -0500

    Adds MethodName and FullMethodName.

---
+++ h-extensions/MethodExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection;
+
+namespace Hylasoft.Extensions
+{
+  public static class MethodExtensions
+  {
+    /// <summary>
+    /// Returns the name of the object and method, in the format of: Object.Method.
+    /// </summary>
+    public static string MethodName(this MethodInfo method)
+    {
+      return MethodName(method, false);
+    }
+
+    /// <summary>
+    /// Returns the name of the object, method, and full namespace of the object.
+    /// </summary>
+    /// <param name="method"></param>
+    /// <returns></returns>
+    public static string FullMethodName(this MethodInfo method)
+    {
+      return MethodName(method, true);
+    }
+
+    private static string MethodName(MethodInfo method, bool fullNamespace)
+    {
+      if (method == null)
+        return string.Empty;
+
+      Type reflected;
+      if ((reflected = method.ReflectedType) == null)
+        return method.Name;
+
+      var methodName = method.Name;
+      var className = fullNamespace
+        ? reflected.FullName
+        : reflected.Name;
+
+      return string.Format("{0}.{1}", methodName, className); 
+    }
+  }
+}

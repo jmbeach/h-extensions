1ce640c2ce712783ba40bbbdb6c2136d11959494
jcooley-hylasoft <j.cooley@hylasoftusa.com>
2016-12-14 11:59:45 -0600

    Adds enumeration extensions.

---
+++ ExtensionTests/EnumerationTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hylasoft.Extensions
+{
+  [TestClass]
+  public class EnumerationTests
+  {
+    [TestMethod]
+    public void EnumerationToCollectionTest()
+    {
+      var testSet = new[]
+      {
+        1, 2, 3, 4
+      };
+
+      var testCollection = testSet.ToCollection();
+      Assert.AreEqual(testCollection.GetType(), typeof(Collection<int>));
+      AssertComparable(testCollection, testSet);
+    }
+
+    [TestMethod]
+    public void EnumerationForEachTest()
+    {
+      var valuesToSet = new List<int>();
+      var initialValues = new[]
+      {
+        1, 2, 3, 4
+      };
+
+      initialValues.ForEach(valuesToSet.Add);
+      AssertComparable(valuesToSet, initialValues);
+    }
+
+    protected void AssertComparable<TVal>(IList<TVal> a, IList<TVal> b)
+    {
+      Assert.AreEqual(a.Count, a.Count);
+
+      for (var i = 0; i < a.Count; i++)
+        Assert.AreEqual(a[i], b[i]);
+    }
+  }
+}
---
+++ h-extensions/EnumerationExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Hylasoft.Extensions
+{
+  public static class EnumerationExtensions
+  {
+    /// <summary>
+    /// Translates any enumeration into a collection of the same type.
+    /// </summary>
+    /// <typeparam name="TVal">The type of object for the collection.</typeparam>
+    /// <param name="valueSet">The IEnumerable values.</param>
+    /// <returns>Returns a Collection<TVal> of valueSet.</TVal></returns>
+    public static Collection<TVal> ToCollection<TVal>(this IEnumerable<TVal> valueSet)
+    {
+      var valueArray = valueSet == null
+        ? new TVal[0]
+        : valueSet.ToArray();
+
+      return new Collection<TVal>(valueArray);
+    }
+
+    /// <summary>
+    /// Performs an action on each value of a set.
+    /// </summary>
+    /// <typeparam name="TVal">The type associated with the set.</typeparam>
+    /// <param name="valueSet">The set to perform actions on.</param>
+    /// <param name="action">The action to perform.</param>
+    public static void ForEach<TVal>(this IEnumerable<TVal> valueSet, Action<TVal> action)
+    {
+      foreach (var value in valueSet)
+        action(value);
+    }
+  }
+}

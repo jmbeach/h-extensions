e49c960e3ca669a2882cc206b8cae502638d0638
jcooley-hylasoft <j.cooley@hylasoftusa.com>
2017-02-15 12:38:00 -0600

    Adds unit tests for type extensions.

---
+++ ExtensionTests/TypeTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hylasoft.Extensions
+{
+  [TestClass]
+  public class TypeTests
+  {
+    protected class TypeTestClass
+    {
+      public int DefaultInt { get; set; }
+
+      public string DefaultStr { get; set; }
+
+      public TypeTestClass DefaultClass { get; set; }
+    }
+
+    [TestMethod]
+    public void DefaultTypeTest()
+    {
+      var testClass = new TypeTestClass();
+      var defaultInt = TypeExtensions.DefaultValue<int>();
+      var defaultString = TypeExtensions.DefaultValue<string>();
+      var defaultClass = TypeExtensions.DefaultValue<TypeTestClass>();
+
+      Assert.AreEqual(testClass.DefaultInt, defaultInt);
+      Assert.AreEqual(defaultString, testClass.DefaultStr);
+      Assert.AreEqual(defaultClass, testClass.DefaultClass);
+
+      Assert.AreEqual(typeof(int).DefaultValue(), testClass.DefaultInt);
+      Assert.AreEqual(typeof(string).DefaultValue(), testClass.DefaultStr);
+      Assert.AreEqual(typeof(TypeTestClass).DefaultValue(), testClass.DefaultClass);
+    }
+  }
+}

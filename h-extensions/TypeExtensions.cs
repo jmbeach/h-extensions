7be1283854fa30dc938ef64161652ff6e8d783a4
jcooley-hylasoft <j.cooley@hylasoftusa.com>
2017-02-15 11:21:04 -0600

    Retrieves default values from types.

---
+++ h-extensions/TypeExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Hylasoft.Extensions
+{
+  public static class TypeExtensions
+  {
+    /// <summary>
+    /// Returns the default value of the type.
+    /// </summary>
+    /// <param name="type">The type to retrieve a default value for.</param>
+    /// <returns>A boxed value of a default instance of this type.</returns>
+    public static object DefaultValue(this Type type)
+    {
+      return type.IsValueType
+        ? Activator.CreateInstance(type)
+        : null;
+    }
+
+    /// <summary>
+    /// Returns default value of a type.
+    /// </summary>
+    /// <typeparam name="TVal">The type to retrieve a default value from.</typeparam>
+    /// <returns>The default value of the provided type.</returns>
+    public static TVal DefaultValue<TVal>()
+    {
+      return (TVal) DefaultValue(typeof (TVal));
+    }
+  }
+}

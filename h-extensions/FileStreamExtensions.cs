893900b5fdff48a6141625f1fbd7673cd90e470b
jmbeach <jaredbeachdesign@gmail.com>
2018-03-29 20:57:30 -0400

    file stream to byte array

---
+++ h-extensions/FileStreamExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace Hylasoft.Extensions
+{
+  public static class FileStreamExtensions
+  {
+    /// <summary>
+    /// Converts the file stream into an array of bytes.
+    /// <param name="stream">Stream to convert into byte array</param>
+    /// </summary>
+    public static byte[] ToByteArray(this FileStream stream)
+    {
+      var bytes = new byte[stream.Length];
+      stream.Read(bytes, 0, bytes.Length);
+      return bytes;
+    }
+  }
+}

5bca20cd7736f45b71f2a7df041d8906de698a84
jmbeach <jaredbeachdesign@gmail.com>
2018-03-29 21:13:37 -0400

    ToByteArray tests

---
+++ ExtensionTests/FileStreamTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hylasoft.Extensions
+{
+  [TestClass]
+  public class FileStreamTests
+  {
+    [TestMethod]
+    public void FileStreamToByteArrayTest()
+    {
+      using (var stream = new FileStream("..\\..\\byte-test-file.txt", FileMode.Open))
+      {
+        var bytes = stream.ToByteArray();
+        Assert.AreEqual(816, bytes.Length);
+      }
+    }
+  }
+}
